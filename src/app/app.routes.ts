0caf6e288bf49e051c033dc3f57a992ef4d03d4b
surendra-ims <surendra@imssoftware.com.np>
2025-09-05 08:18:34 +0545

    Language Changes, added items

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,22 @@
 
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { OrderComponent } from './components/order/order.component';
-import { DineInComponent } from './components/dine-in/dine-in.component';
+import { OrderComponent } from './components/order/order/order.component';
 import { BookingComponent } from './components/booking/booking.component';
 import { LocationComponent } from './components/location/location.component';
 import { GetAppComponent } from './components/get-app/get-app.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { OrderLocationComponent } from './components/order/order-location/order-location.component';
+import { DineInAndTakeawayComponent } from './components/dine-in/dine-in-and-takeaway/dine-in-and-takeaway.component';
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'order', component: OrderComponent },
-  { path: 'dine-in', component: DineInComponent },
+  { path: 'order/:branchId', component: OrderComponent },
+  { path: 'dine-in-and-takeaway', component: DineInAndTakeawayComponent },
   { path: 'booking', component: BookingComponent },
   { path: 'location', component: LocationComponent },
   { path: 'get-app', component: GetAppComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'order', component: OrderLocationComponent },
 ];
