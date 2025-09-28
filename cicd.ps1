89de4c2fefc07252b2a9403aae9f763494a4a380
surendra-ims <surendra@imssoftware.com.np>
2025-09-28 07:09:29 +0545

    Hidden Delivery Option

---
+++ cicd.ps1
@@ -0,0 +1,28 @@
+$sourcePath = "D:\Angular\DineIn"
+$deployPath = "D:\Angular\DineIn-ghPage"
+$distPath   = "$sourcePath\dist\myapp\browser"
+
+
+Set-Location $sourcePath
+git add .
+$commitMsg = Read-Host "Enter commit message for source repo"
+git commit -m "$commitMsg"
+git push
+
+ng build --configuration production
+
+Set-Location $deployPath
+Get-ChildItem -Path $deployPath -Recurse | Where-Object {
+    $_.Name -notin @(".git", ".gitignore", ".nojekyll", "CNAME")
+} | Remove-Item -Recurse -Force
+
+Copy-Item -Path "$distPath\*" -Destination $deployPath -Recurse
+
+Copy-Item "$deployPath\index.html" "$deployPath\404.html" -Force
+
+git add .
+$commitMsg = Read-Host "Enter commit message for gh-pages repo"
+git commit -m "$commitMsg"
+git push
+
+Write-Host "✅ Deployment complete!"
